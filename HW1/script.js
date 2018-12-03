1ed849e51bf4d01edb3ca9b1bd25218a31120276
IlyaBielov <ibioriginalibi@gmail.com>
2018-12-03 19:57:51 +0200

    HW1

---
+++ HW1/script.js
@@ -0,0 +1,11 @@
+let x = 5.24;
+let y = '3.64';
+let z = 10.11;
+
+y = parseFloat(y);
+
+let result = x + y + z;
+
+document.write("<p>The result of adding numbers: "
+ +x+ ' + ' +y+ ' + ' +z+ ' = ' + 
+ "<span style=color:blue>" +result+ "</span></p>");
